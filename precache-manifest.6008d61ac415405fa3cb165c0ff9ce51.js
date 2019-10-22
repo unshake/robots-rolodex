46a1ea6d5232e1aa147a1de1719d70dd6a550b1a
Edgar Martinez <edgarmartinezme@gmail.com>
2019-10-22 11:32:23 -0600

    Updates

---
+++ precache-manifest.6008d61ac415405fa3cb165c0ff9ce51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f95b84e9f94e21cff6db5b560bd02c",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "524ab1c4b7d53d8b05f4",
+    "url": "/robots-rolodex/static/css/main.ecfd81de.chunk.css"
+  },
+  {
+    "revision": "157cdb102858af4505a9",
+    "url": "/robots-rolodex/static/js/2.6da1f240.chunk.js"
+  },
+  {
+    "revision": "524ab1c4b7d53d8b05f4",
+    "url": "/robots-rolodex/static/js/main.76475846.chunk.js"
+  },
+  {
+    "revision": "e8c79aec41aa7993f329",
+    "url": "/robots-rolodex/static/js/runtime-main.a5cc6734.js"
+  }
+]);
